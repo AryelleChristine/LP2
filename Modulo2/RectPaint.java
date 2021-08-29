93aece229a3e6928e456c78c8e4ff148a4527879
AryelleChristine <88325814+AryelleChristine@users.noreply.github.com>
2021-08-29 17:33:12 -0300

    Update RectPaint.java

--- Modulo2/RectPaint.java
+++ Modulo2/RectPaint.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class RectPaintApp {
+public class RectPaintApp {
 	public static void main (String[] args) {
         	RectPaintFrame frame = new RectPaintFrame();
         	frame.setVisible(true);
@@ -22,9 +22,9 @@ class RectPaintFrame extends JFrame {
         	);
         this.setTitle("Retangulo");
         this.setSize(300, 300);
-        this.r1 = new Rect(100,150, 110,50,250,30,0);
-	this.r2 = new Rect(250,270, 100,50,30,0,250);
-	this.r3 = new Rect(350,370, 200,80,0,250,205);
+        this.r1 = new Rect(100,150, 110,50,250,30,0,0,203,55);
+	this.r2 = new Rect(250,270, 100,50,30,0,250,0,0,250);
+	this.r3 = new Rect(350,370, 200,80,0,250,205,200,30,10);
     	}
 
     	public void paint (Graphics g) {
@@ -39,8 +39,9 @@ class Rect {
     	int x, y;
     	int w, h;
 	int r,g2,b;
+	int r2,g3,b2;
 
-    	Rect (int x, int y, int w, int h,int r,int g2,int b) {
+    	Rect (int x, int y, int w, int h,int r,int g2,int b,int r2,int g3,int b2) {
         	this.x = x;
         	this.y = y;
         	this.w = w;
@@ -48,6 +49,9 @@ class Rect {
 		this.r = r;
 		this.g2 = g2;
 		this.b = b;
+		this.r2 = r2;
+		this.g3 = g3;
+		this.b2 = b2;
     	}
 
    	void print () {
@@ -60,7 +64,7 @@ class Rect {
 
 		g.setColor(new Color(this.r,this.g2,this.b));
 		g.fillRect (this.x, this.y, this.w, this.h);
-		g.setColor(new Color(this.r,this.g2,this.b));
+		g.setColor(new Color(this.r2,this.g3,this.b2));
         	g2d.drawRect(this.x,this.y, this.w,this.h);	
 	
    	 }
