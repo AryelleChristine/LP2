3482cd2d0b29614df32070b1d251b4d65b8a197d
AryelleChristine <88325814+AryelleChristine@users.noreply.github.com>
2021-09-19 15:38:49 -0300

    Update and rename quadrado.c to Square.c

--- Modulo1/Square.c
+++ Modulo1/Square.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 typedef struct{
-	int x,y,w,h;
-}Quadrado;
+	int x,y;
+	int w,h;
+}Square;
 
-void print(Quadrado* q){
+void print(Square* q){
 	printf("Quadrado de tamanho (%d,%d) na posicao (%d,%d)\n",
-	q->w,q->h,q->x,q->y);	
+	 q->w,q->h,q->x,q->y);	
 }
 
 void main (void){
-	Quadrado q1 = {2,2,50,50};
+	Square q1 = {2,2,50,50};
 	print(&q1);
 }
