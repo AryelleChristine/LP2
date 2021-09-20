847b8ff2a900010f8f8868d763068e69fa51c724
AryelleChristine <88325814+AryelleChristine@users.noreply.github.com>
2021-09-20 09:19:58 -0300

    Update Square.java

--- Modulo2/figures/Square.java
+++ Modulo2/figures/Square.java
@@ -5,16 +5,16 @@ import java.awt.*;
 public class Square {
     int x, y;
     int w, h;
-    int r,g2,b;
+    int rf,gf,bf;
 
-    public Square (int x, int y, int w, int h,int r,int g2,int b) {
+    public Square (int x, int y, int w, int h,int rf,int gf,int bf) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-	this.r = r;
-        this.g2 = g2;
-        this.b = b;
+	this.rf = rf;
+        this.gf = gf;
+        this.bf = bf;
     }
 
     public void print () {
@@ -24,8 +24,9 @@ public class Square {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        g2d.drawRect(this.x,this.y, this.w,this.h);
+
+        g.setColor(new Color(this.rf,this.gf,this.bf));
 	g.fillRect(this.x,this.y,this.w,this.h);
-	g.setColor(new Color(this.r,this.g2,this.b));
+	g2d.drawRect(this.x,this.y, this.w,this.h);
     }
 }
