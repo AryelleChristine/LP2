588c676633e2654bf89154f8fb05edd88aeee94c
AryelleChristine <88325814+AryelleChristine@users.noreply.github.com>
2021-09-20 09:19:11 -0300

    Update Rect.java

--- Modulo2/figures/Rect.java
+++ Modulo2/figures/Rect.java
@@ -5,12 +5,16 @@ import java.awt.*;
 public class Rect {
     int x, y;
     int w, h;
+    int rf,gf,bf;
 
-    public Rect (int x, int y, int w, int h) {
+    public Rect (int x, int y, int w, int h,int rf,int gf,int bf) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+	    this.rf = rf;
+        this.gf = gf;
+        this.bf = bf;
     }
 
     public void print () {
@@ -20,6 +24,9 @@ public class Rect {
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
+
         g2d.drawRect(this.x,this.y, this.w,this.h);
+	    g.setColor(new Color(this.rf,this.gf,this.bf));
+	    g.fillRect(this.x,this.y,this.w,this.h);
     }
 }
