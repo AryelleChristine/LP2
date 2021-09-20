c49d8c28281e7d5bb614a88264b017aad87955eb
AryelleChristine <88325814+AryelleChristine@users.noreply.github.com>
2021-09-20 10:02:59 -0300

    Update ListAppH.java

--- Figure/ListAppH.java
+++ Figure/ListAppH.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-import figuress.*;
+import figures.*;
 
 class ListAppH {
     public static void main (String[] args) {
