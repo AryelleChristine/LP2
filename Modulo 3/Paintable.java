55bca5d435610e1a36fe80d1f0d0555af07237c2
AryelleChristine <88325814+AryelleChristine@users.noreply.github.com>
2021-10-15 16:35:30 -0300

    Create Paintable.java

---
+++ Modulo 3/Paintable.java
@@ -0,0 +1,32 @@
+interface Paintable{ //pintavel
+  int topaint(void); //pintar
+  int sand (void); //lixar
+}
+
+class Wall implements Paintable{
+  int topaint (void){
+    ...
+    }
+  int sand (void){
+    ...
+    }
+}
+
+class Nail implements Paintable{
+  int topaint (void){
+    ...
+    }
+  int sand (void){
+    ...
+    }
+}
+
+
+class Door implements Paintable{
+  int topaint(void){
+    ...
+    }
+  int sand (void){
+    ...
+    }
+}
