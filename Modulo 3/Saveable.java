226d7521efa9318aa4e9dcbb6af685833e9ab02d
AryelleChristine <88325814+AryelleChristine@users.noreply.github.com>
2021-10-15 16:22:05 -0300

    Create Saveable.java

---
+++ Modulo 3/Saveable.java
@@ -0,0 +1,32 @@
+interface Saveable{
+  void open(int a);
+  int save(int o);
+}
+
+class Refrigerator implements Saveable{
+  void open (int a){
+    ...
+   }
+  int save (int o){
+    ...
+   }
+}
+
+class Cabinet implements Saveable{
+  void open(int a){
+    ...
+    }
+  int save (int o){
+    ...
+    }
+}
+
+
+class Handbag implements Saveable{
+  void open(int a){
+    ...
+    }
+  int save(int o){
+    ...
+    }
+}
